1547d91d2b046946d42d231927d927a76cd4a65e
realVato <44781061+realVato@users.noreply.github.com>
2019-06-23 05:37:45 +0300

    #skills and #intro bg change on a set interval

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+// Skills
+
 (function() {
 
-    var skills = ['HTML/CSS', 'SASS', 'JavaScript', 'jQuery', 'Wordpress'];
+    var skills = ['HTML/CSS', 'SASS', 'JavaScript', 'jQuery'];
     var counter = 0;
     
     var skill = $('#skills');
@@ -9,13 +11,42 @@
         
         skill.text(skills[counter++]);
         
-        if(counter >= skills.length){
+        if (counter >= skills.length){
             counter = 0;
         }
-    }, 800)
+    }, 1000)
 })();
 
+
+// Cover changes as skills do
+
+(function() {
+
+  var backgrounds = [];
+  backgrounds[0] = '../img/html.jpg';
+  backgrounds[1] = '../img/scss.jpg';
+  backgrounds[2] = '../img/javascript.jpg';
+  backgrounds[3] = '../img/jquery.jpg';
+  var counter = 0;
+
+  var background = document.getElementById('intro');
+  
+  setInterval(function(){
+    
+    background.style.backgroundImage = 'url(' + backgrounds[counter++] + ')';
+
+    if (counter >= backgrounds.length){
+        counter = 0;
+  }
+
+  }, 1000)
+})();
+
+
+// Hover down
+
 $('.about-me a').on('click', function(event) {
+
     if (this.hash !== '') {
       event.preventDefault();
   
