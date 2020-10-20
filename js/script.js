47dc1b87a1994be20535a016b0cbe8b7f07168b3
Tomas Talalas <44781061+realVato@users.noreply.github.com>
2020-10-20 14:51:26 +0300

    temporary task project

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 
 (() => {
 
-  const skills = ['HTML/CSS', 'Bootstrap/Materialize', 'SASS', 'JavaScript', 'React', 'PHP'];
+  const skills = ['HTML/CSS', 'Bootstrap/Materialize', 'SASS', 'JavaScript', 'PHP'];
   let counter = 0;
 
   const skill = document.getElementById('skills');
